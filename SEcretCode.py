04b2464c38cdf37bf7fb06602280829e85bd5d0d
delfina <delfinabilbaoa@gmail.com>
2016-09-10 16:55:44 -0300

    subo archivo que genera secuencias random

---
+++ SEcretCode.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import random
+
+
+
+def eleccion_procesador():
+	grupo = []
+
+	colores = {1:"rojo", 2: "amarillo", 3: "azul", 4: "verde", 5:"naranja" , 6: "violeta", 7 : "blanco", 8:"negro"}
+
+	for i in range  (4):
+	
+		grupo.append (colores [random.randint (1,8)])
+
+	return grupo
+
+print eleccion_procesador ()
+
+def eleccion_humano ():
+	
+	
+	# cuando hace click, se colorean, se cargan en la fila
+
+
+	
+	
+	
+	
+	
+
+
+
+
+
